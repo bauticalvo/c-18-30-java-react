5e57f5a12cf5f97b7779faaab65c4137ea1349b8
Omar <omar@gmail.com>
2024-05-31 10:07:15 -0400

    Added controllers

---
+++ api/Back/src/main/java/com/telemedicina/repositorys/DoctorRepository.java
@@ -0,0 +1,11 @@
+package com.telemedicina.repositorys;
+
+import com.telemedicina.entitys.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository 
+        extends JpaRepository <Doctor, Integer> {
+    
+}
--- api/Back/src/main/java/com/telemedicina/repositorys/UserRepository.java
+++ api/Back/src/main/java/com/telemedicina/repositorys/UserRepository.java
@@ -2,7 +2,10 @@ package com.telemedicina.repositorys;
 
 import com.telemedicina.entitys.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository <User, Integer>{
+@Repository
+public interface UserRepository 
+        extends JpaRepository <User, Integer>{
     
 }
