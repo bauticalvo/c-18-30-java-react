cd22e1d7106768d2c429cb81d41c02809c683eb6
LOLA-1980 <109870340+LOLA-1980@users.noreply.github.com>
2024-05-31 00:21:52 +0000

    [Creacion del componente DoctorsList para poder hacer fetch de toda la lista de doctores mediante un filtardo por Especialidad y Ciudad que se hace por medio de una barra de busqueda]

--- api/Front/src/App.jsx
+++ api/Front/src/App.jsx
@@ -1,30 +1,23 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './components/Home'
-import ProRegister from './components/ProRegister/ProRegister'
-import Profile from './components/Profile'
-import Navbar from './components/Navbar'
-import UserRegister from './components/UserRegister/UserRegister'
-import Login from './components/Login'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import DoctorsList from './components/DoctorsList';
+import Navbar from './components/Navbar';
 
 function App() {
-
-  return (
-    <>
-      <div>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path='/' element={<Home/>} />  
-          <Route path='/ProfesionalRegister' element={<ProRegister/>} />  
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/UserRegister' element={<UserRegister />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </div>
-
-    </>
-  )
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<><Navbar /><Login /></>} />
+                <Route path="/profile" element={<><Navbar /><Profile /></>} />
+                <Route path="/doctors" element={<><Navbar /><DoctorsList /></>} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
